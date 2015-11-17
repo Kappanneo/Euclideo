fa33021c46032cd25a4f25b71710ae169c9430c7
Kappanneo <claudio_pannacci@libero.it>
2015-11-17 18:57:21 +0100

    ABS as define.

--- euclideo.c
+++ euclideo.c
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
 
   long a= unint(argv[1]);
   long b= unint(argv[2]);
-  long a2= a >= 0? a: -a;
-  long b2= b >= 0? b: -b;
+  long a2= ABS(a);
+  long b2= ABS(b);
   long vid= a2 > b2? (argc==3? a2: a): (argc==3? b2: b);
   long vis= a2 > b2? (argc==3? b2: b): (argc==3? a2: a);
   int q;
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
       t[1]= t[0]-q*t[1];
       t[0]= temp;
 
-      printf("%ld= %d* %ld %c%ld ", vid, q, vis, vid%vis<0?'-':'+', vid%vis<0? -vid%vis: vid%vis);
+      printf("%ld= %d* %ld %c%ld ", vid, q, vis, vid%vis<0?'-':'+', ABS(vid%vis));
       if(argc==4)
 	if(unint(argv[3])!=0)
 	  printf("(%d, %d)", s[0], t[0]);
@@ -39,13 +39,13 @@ int main(int argc, char *argv[])
       vid= temp;
     }
       
-  printf("\nMCD(%ld,%ld)= %ld\n\n",a,b,vid >0? vid: -vid);
+  printf("\nMCD(%ld,%ld)= %ld\n\n", a, b, ABS(vid));
       
   if(argc==4)
     if(unint(argv[3])!=0)
       {
 	long c= unint(argv[3]);
-	printf("%ldx %c%ldy =%ld", a, b<0?'-':'+', b<0? -b: b, c);
+	printf("%ldx %c%ldy = %ld", a, b<0?'-':'+', b2, c);
 	printf("%sha soluzioni\n\n", c%vid?" non ":" ");
 	    
 	if(c%vid==0)
@@ -53,8 +53,8 @@ int main(int argc, char *argv[])
 	    long xo= a2 > b2? s[0]*c/vid : t[0]*c/vid;
 	    long yo= a2 > b2? t[0]*c/vid : s[0]*c/vid;
 		
-	    printf("x= %ld %c%ldk\n", xo, b/vid<0?'-':'+', b/vid<0? -b/vid: b/vid); 
-	    printf("y= %ld %c%ldk\n\n", yo, a/vid>0?'-':'+', a/vid>0? a/vid: -a/vid);
+	    printf("x= %ld %c%ldk\n", xo, b/vid<0?'-':'+', ABS(b/vid)); 
+	    printf("y= %ld %c%ldk\n\n", yo, a/vid>0?'-':'+', ABS(a/vid));
 	  }
       }
 
